ece755bbdc4e959f4856e710a587970f54c596ee
markpan07 <markpan07@mail.ru>
2023-12-09 17:30:50 +0300

    2.1 is done

---
+++ src/main/resources/liquibase/notification_task.sql
@@ -0,0 +1,12 @@
+-- liquibase formatted sql
+
+-- changeset mpankov:1
+CREATE TABLE notification_task
+(
+    id       SERIAL PRIMARY KEY,
+    chatId   BIGINT                      NOT NULL,
+    text     TEXT                        NOT NULL,
+    date_time TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE INDEX date_time_index ON notification_task(date_time)
