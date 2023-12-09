25f65ef75aba4b7cbecb751fb24c14268bddcace
markpan07 <markpan07@mail.ru>
2023-12-09 16:52:37 +0300

    1.4 is done

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,9 +3,12 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +16,8 @@ import java.util.List;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
+    @Value("${greetings}")
+    private String greetings;
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
@@ -28,9 +33,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if(update.message().text().equals("/start")) {
+                SendMessage message = new SendMessage(update.message().chat().id(), greetings);
+                SendResponse response = telegramBot.execute(message);
+            }
         });
-        return UpdatesListener.CONFIRMED_UPDATES_ALL;
+
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
 }
